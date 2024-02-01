cae473a238c88245aca52aca2e92660e0025b8a9
Syahrul Ilham Muttaqien <syahrulilham62@gmail.com>
2024-02-02 04:55:02 +0700

    scan component

--- src/main/java/com/api/employee/EmployeeApplication.java
+++ src/main/java/com/api/employee/EmployeeApplication.java
@@ -2,8 +2,10 @@ package com.api.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.api.employee", "com.api.common.beans" })
 public class EmployeeApplication {
 
 	public static void main(String[] args) {
